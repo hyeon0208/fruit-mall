ee3477fc748516cca176eaf480e73d927e0c6e17
Hyeon0208 <dmsgktn0208@gmail.com>
2023-07-30 18:20:14 +0900

    fix : term_flag 반환 타입 Integer로 변경

--- src/main/java/com/fruit/mall/user/dto/Term.java
+++ src/main/java/com/fruit/mall/user/dto/Term.java
@@ -10,10 +10,10 @@ public class Term {
     private Long term_id_no;
     private Long user_id_no;
     private String term_name;
-    private boolean term_flag;
+    private Integer term_flag;
 
     @Builder
-    public Term(Long user_id_no, String term_name, boolean term_flag) {
+    public Term(Long user_id_no, String term_name, Integer term_flag) {
         this.user_id_no = user_id_no;
         this.term_name = term_name;
         this.term_flag = term_flag;
