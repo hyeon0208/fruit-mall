eda5ba591d336a2a5bd64a3560f6c11fad1a858b
Hyeon0208 <dmsgktn0208@gmail.com>
2023-07-31 00:35:37 +0900

    refactor : build 수정

--- src/main/java/com/fruit/mall/user/dto/User.java
+++ src/main/java/com/fruit/mall/user/dto/User.java
@@ -11,15 +11,18 @@ import java.io.Serializable;
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+
+    private Long user_id_no;
     private String user_email;
     private String user_name;
     private String user_pwd;
     private Role user_status;
 
     @Builder
-    public User(String user_email, String user_name, String user_pwd) {
+    public User(String user_email, String user_name, String user_pwd, Role user_status) {
         this.user_email = user_email;
         this.user_name = user_name;
         this.user_pwd = user_pwd;
+        this.user_status = user_status;
     }
 }
