533dcab62a131ead6e5fa669dccbf0261e97c735
Hyeon0208 <dmsgktn0208@gmail.com>
2023-07-30 20:50:09 +0900

    feat : mybatis enum 타입사용을 위한 Handler

--- src/main/java/com/fruit/mall/user/RoleTypeHandler.java
+++ src/main/java/com/fruit/mall/user/RoleTypeHandler.java
@@ -1,34 +1,35 @@
 package com.fruit.mall.user;
 
-import org.apache.ibatis.type.BaseTypeHandler;
-import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.*;
 
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@MappedTypes(Role.class)
 public class RoleTypeHandler extends BaseTypeHandler<Role> {
+
     @Override
     public void setNonNullParameter(PreparedStatement ps, int i, Role parameter, JdbcType jdbcType) throws SQLException {
-        ps.setString(i, parameter.toString());
+        ps.setString(i, parameter.getKey());
     }
 
     @Override
     public Role getNullableResult(ResultSet rs, String columnName) throws SQLException {
-        String value = rs.getString(columnName);
-        return value != null ? Role.valueOf(value) : null;
+        String key = rs.getString(columnName);
+        return key == null ? null : Role.search(key);
     }
 
     @Override
     public Role getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-        String value = rs.getString(columnIndex);
-        return value != null ? Role.valueOf(value) : null;
+        String key = rs.getString(columnIndex);
+        return key == null ? null : Role.search(key);
     }
 
     @Override
     public Role getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
-        String value = cs.getString(columnIndex);
-        return value != null ? Role.valueOf(value) : null;
+        String key = cs.getString(columnIndex);
+        return key == null ? null : Role.search(key);
     }
 }
