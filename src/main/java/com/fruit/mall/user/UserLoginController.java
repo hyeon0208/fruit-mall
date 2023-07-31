a099e44216ebf547100d4c8a06cafd4f8d7050d8
Hyeon0208 <dmsgktn0208@gmail.com>
2023-07-31 20:43:50 +0900

    feat(UserLoginController) : 로그인 유효성 검사 추가

--- src/main/java/com/fruit/mall/user/UserLoginController.java
+++ src/main/java/com/fruit/mall/user/UserLoginController.java
@@ -10,29 +10,36 @@ import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/user")
 public class UserLoginController {
 
     private final UserService userService;
     private final static String LOGIN_USER = "authentic";
 
-    @PostMapping("/login")
-    public String loginForm(@ModelAttribute User user, HttpServletRequest request) {
+    @PostMapping("/user/login")
+    @ResponseBody
+    public String loginForm(@RequestBody User user, HttpServletRequest request) {
         User loginUser = userService.selectUserByUserEmail(user.getUser_email());
-        if (!userService.loginCheck(user.getUser_email(),user.getUser_pwd())) {
-            // 로그인 실패 시
+        if (!userService.loginCheck(user.getUser_pwd(), loginUser)) {
+            return "이메일 또는 비밀번호가 일치하지 않습니다.";
         }
         HttpSession session = request.getSession();
         session.setAttribute(LOGIN_USER, loginUser);
-        return "redirect:/";
+        return "success";
     }
 
-    @PostMapping("/logout")
+    @PostMapping("/user/logout")
     public String logout(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
         if (session != null) {
             session.invalidate();
         }
         return "redirect:/";
+    }
+
+    @PostMapping("/user/check-login")
+    @ResponseBody
+    public String checkLoginPwd(@RequestParam String user_email) {
+        String findEmail = userService.selectEmailByUserEmail(user_email);
+        return findEmail == null ? "이메일이 존재하지 않습니다." : "";
     }
 }
