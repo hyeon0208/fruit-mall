c02f27c44db9abf67c106fa002f668d4d0ddb53c
Hyeon0208 <dmsgktn0208@gmail.com>
2023-08-03 12:04:27 +0900

    feat : Product insert 기능 구현

--- src/main/java/com/fruit/mall/admin/product/ProductMapper.java
+++ src/main/java/com/fruit/mall/admin/product/ProductMapper.java
@@ -6,10 +6,5 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface ProductMapper {
 
-    void insertProduct(@Param("categoryId") Long categoryId,
-                       @Param("productName") String productName,
-                       @Param("productPrice") int productPrice,
-                       @Param("productStock") int productStock,
-                       @Param("productDescription") String productDescription,
-                       @Param("productDiscount") int productDiscount);
+    void insertProduct(Product product);
 }
--- src/main/java/com/fruit/mall/admin/product/ProductService.java
+++ src/main/java/com/fruit/mall/admin/product/ProductService.java
@@ -11,7 +11,7 @@ public class ProductService implements ProductMapper {
     private final ProductMapper productMapper;
 
     @Override
-    public void insertProduct(Long categoryId, String productName, int productPrice, int productStock, String productDescription, int productDiscount) {
-        productMapper.insertProduct(categoryId, productName, productPrice, productStock, productDescription, productDiscount);
+    public void insertProduct(Product product) {
+        productMapper.insertProduct(product);
     }
 }
