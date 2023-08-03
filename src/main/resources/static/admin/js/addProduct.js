0502e836b07455fb51f829ee820cbe944fc2dfcb
Hyeon0208 <dmsgktn0208@gmail.com>
2023-08-03 18:51:22 +0900

    fix : addProduct.js blobid 파일 전송 문제 수정

--- src/main/resources/static/admin/js/addProduct.js
+++ src/main/resources/static/admin/js/addProduct.js
@@ -17,6 +17,7 @@ function uploadImage() {
         reader.onload = (e) => {
             $('#previewImage').attr('src', e.target.result);
         }
+        reader.readAsDataURL(file); // file을 Data URL로 읽어오고, 읽기가 완료되면 이미지 미리보기로 표시
     }
 }
 
@@ -101,8 +102,14 @@ $(() => {
         images_upload_handler(blobInfo, success) { // 이미지를 업로드하는 핸들러 함수
             // blobInfo : TinyMCE에서 이미지 업로드 시 사용되는 정보를 담고 있는 객체
             const file = new File([blobInfo.blob()], blobInfo.filename());
-            imageFiles.push(file);
-            success(URL.createObjectURL(file)); // Blob 객체의 임시 URL을 생성해 이미지 미리보기 적용
+            const fileName = blobInfo.filename();
+
+            if (fileName.includes("blobid")) {
+                success(URL.createObjectURL(file));
+            } else {
+                imageFiles.push(file);
+                success(URL.createObjectURL(file)); // Blob 객체의 임시 URL을 생성해 이미지 미리보기 적용
+            }
         }
     });
 
