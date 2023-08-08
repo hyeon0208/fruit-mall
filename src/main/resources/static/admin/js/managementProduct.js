a76648cc0a7774e557dc2ab90048dc3fcf0bbc9f
Hyeon0208 <dmsgktn0208@gmail.com>
2023-08-08 21:58:14 +0900

    feat : 비동기 방식으로 동적 쿼리 조회 + 조건별 페이징 적용

--- src/main/resources/static/admin/js/managementProduct.js
+++ src/main/resources/static/admin/js/managementProduct.js
@@ -1,10 +1,29 @@
-// 상태, 분류, 검색 버튼 값 초기 세팅
+// 상태, 분류, 검색 버튼, 현재 페이지 값 초기 세팅
 let selectedStatus, selectedCategory, selectedSearch;
+let currentPage= 1;
+
+// 이전 페이지 버튼
+$(document).on('click', '.prev', () => {
+    currentPage -= 1;
+    updateProductList();
+});
+
+// 페이지 번호 버튼
+$(document).on('click', '.numberBtn', (e) => {
+    currentPage = $(e.currentTarget).attr("value");
+    updateProductList();
+});
+
+// 다음 페이지 버튼
+$(document).on('click', '.next', () => {
+    currentPage += 1;
+    updateProductList();
+});
 
 // 수정 버튼
 $(document).on("click", ".editBtn", (e) => {
     const btn = $(e.currentTarget);
-    const productId = btn.data("product-id-edit");
+    const productId = btn.val();
     const editUrl = `/admin/edit/product/${productId}`;
     window.location.href = editUrl;
 });
@@ -14,13 +33,6 @@ $(document).on("click", "#allCheck", () => {
     // chk1 체크박스의 체크 여부에 따라 모든 체크박스 상태를 변경
     const isChecked = $(this).prop("checked");
     $("input[type='checkbox']").prop("checked", isChecked);
-});
-
-// 검색 버튼
-$(document).on("click", "#searchBtn", () => {
-    const name = $("#searchCond").val();
-    $("#searchCondHid").val(name);
-    $("#searchForm").submit();
 });
 
 // $개 보기 셀렉트 박스 클릭 이벤트
@@ -39,27 +51,28 @@ $(document).on("click", "#excelDownloadBtn", () => {
     link.click();
 });
 
-
-// 상태 버튼 클릭 시
+// 게시상태 버튼
 $(document).on("click", ".statusBtn", (e) => {
     selectedStatus =  $(e.currentTarget).val();
     updateProductList();
 });
 
+// 카테고리 분류 버튼
 $(document).on("click", ".categoryBtn", (e) => {
     selectedCategory =  $(e.currentTarget).val();
     updateProductList();
 });
 
+// 검색어 입력 버튼
 $(document).on("click", "#searchBtn", () => {
-    selectedSearch = $("#searchBtn").val();
+    selectedSearch = $("#searchCond").val();
     updateProductList();
 });
 
 // 판매 중지 버튼 클릭 이벤트
 $(document).on("click", ".stopSaleBtn", (e) => {
     const btn = $(e.currentTarget);
-    const productId = btn.data("product-id");
+    const productId = btn.val();
 
     axios({
         method: "post",
@@ -86,9 +99,7 @@ $(document).on("click", ".stopSaleBtn", (e) => {
     })
 });
 
-// document.ready()
 $(() => {
-
     // 상품 가격 천자리 구분 기호 표시
     $(".productPrice").each((index, element) => {
         const price = $(element).text();
@@ -98,7 +109,7 @@ $(() => {
     // 새로 고침 시에도 판매 중지 열의 시간 표시
     $(".stopSaleBtn").each((index, e) => {
         const btn = $(e);
-        const productId = btn.data("product-id");
+        const productId = btn.val();
         const status = localStorage.getItem(productId);
         if (status === "Status_SaleOut") {
             btn.hide();
@@ -125,36 +136,85 @@ function updateProductList() {
         params: {
             status: selectedStatus || null,
             category: selectedCategory || null,
-            searchCond: selectedSearch || null
+            searchCond: selectedSearch || null,
+            pageNum: currentPage || 1
         }
     }).then((res) => {
-        const products = res.data.products;
-        const tableRows = products.map((product) => { //
-            return `
-                    <tr>
-                        <td>
-                            <input type="checkbox">
-                        </td>
-                        <td>${product.productId}</td>
-                        <td class="product_Status">${product.productSaleStatus}</td>
-                        <td>${getCategoryName(product.categoryId)}</td>
-                        <td>${product.productName}</td>
-                        <td>${(product.productPrice * (1 - (product.productDiscount / 100))).toLocaleString()}원</td>
-                        <td>${product.productDiscount > 0 ? `${product.productDiscount}%` : '-'}</td>
-                        <td name="찜수">-</td>
-                        <td name="결제횟수">-</td>
-                        <td name="리뷰수">-</td>
-                        <td>${new Date(product.productCreatedAt).toLocaleDateString("ko-KR", { year: 'numeric', month: '2-digit', day: '2-digit' }).replaceAll(". ", ".").substring(0, 10)}</td>
-                        <td>
-                            <button class="editBtn" data-product-id-edit="${product.productId}">수정</button>
-                        </td>
-                        <td>
-                            <button class="stopSaleBtn" data-product-id="${product.productId}">중지</button>
-                            <span class="updateTime" style="display: none;"></span>
-                        </td>
-                    </tr>
-                    `;
+
+        // .product-table 클래스 내부의 tbody 내부 내용 교체
+        const products = res.data.list;
+        const tableRows = products.map((product) => {
+            $(".margin .bold").text(res.data.total);
+
+            let stopBtn = $(`<button class="stopSaleBtn" data-product-id="${product.productId}">중지</button>`);
+            let updateTime = "";
+
+            if (product.productSaleStatus === "판매중지") {
+                stopBtn = $("");
+                updateTime = $(`<span class="updateTime">${new Date(product.productUpdatedAt).toLocaleDateString("ko-KR", { year: 'numeric', month: '2-digit', day: '2-digit' }).replaceAll(". ", ".").substring(0, 10)}</span>`);
+            }
+
+            return $("<tr>")
+                .append($("<td>").html(`<input type="checkbox">`))
+                .append($("<td>").text(product.productId))
+                .append($("<td>").addClass("product_Status").text(product.productSaleStatus))
+                .append($("<td>").text(getCategoryName(product.categoryId)))
+                .append($("<td>").text(product.productName))
+                .append($("<td>").text((product.productPrice * (1 - (product.productDiscount / 100))).toLocaleString() + "원"))
+                .append($("<td>").text(product.productDiscount > 0 ? `${product.productDiscount}%` : "-"))
+                .append($("<td>").attr("name", "찜수").text("-"))
+                .append($("<td>").attr("name", "결제횟수").text("-"))
+                .append($("<td>").attr("name", "리뷰수").text("-"))
+                .append($("<td>").text(new Date(product.productCreatedAt).toLocaleDateString("ko-KR", { year: 'numeric', month: '2-digit', day: '2-digit' }).replaceAll(". ", ".").substring(0, 10)))
+                .append($("<td>").html(`<button class="editBtn" value="${product.productId}">수정</button>`))
+                .append($("<td>").append(stopBtn).append(updateTime));
         });
-        $(".product-table tbody").html(tableRows.join(''));
+        $(".product-table tbody").empty().append(tableRows);
+
+        // .pagination 클래스 태그 내부 내용 교체
+        const paginationDiv = $('.pagination');
+        const numbersDiv = $('<p>').addClass('numbers');
+        paginationDiv.empty();
+
+        const totalData = res.data.pages;
+        const totalPages = res.data.navigatepageNums;
+        const currentPage = res.data.pageNum;
+
+        // 이전 페이지 버튼
+        if (res.data.hasPreviousPage) {
+            const prevBtn = $('<a>')
+                .attr('href', '#')
+                .addClass('prev')
+                .attr('value', res.data.prePage)
+                .html('<span class="material-symbols-outlined">chevron_left</span>');
+            paginationDiv.append(prevBtn);
+        }
+
+        totalPages.forEach((pageNumber) => {
+            if (pageNumber <= totalData) {
+                const numberBtn = $('<a>')
+                    .text(pageNumber)
+                    .attr('href', '#')
+                    .addClass('numberBtn')
+                    .attr('value', pageNumber);
+
+                if (pageNumber === currentPage) {
+                    numberBtn.addClass('bold').addClass("large-text").css("font-size", "16px");
+                }
+                numbersDiv.append(numberBtn);
+            }
+        });
+        paginationDiv.append(numbersDiv);
+
+        // 다음 페이지 버튼
+        if (res.data.hasNextPage) {
+            const nextBtn = $('<a>')
+                .attr('href', '#')
+                .addClass('next')
+                .attr('value', res.data.nextPage)
+                .html('<span class="material-symbols-outlined">chevron_right</span>');
+            paginationDiv.append(nextBtn);
+        }
     })
 }
+
