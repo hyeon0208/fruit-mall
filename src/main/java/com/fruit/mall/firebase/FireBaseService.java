aaba2665a770ec2de68c6767bddc32d8f7e9618d
Hyeon0208 <dmsgktn0208@gmail.com>
2023-08-10 12:16:38 +0900

    fix : 중복된 파일이름 저장 시 파일이름 뒤에 (+1) 추가

--- src/main/java/com/fruit/mall/firebase/FireBaseService.java
+++ src/main/java/com/fruit/mall/firebase/FireBaseService.java
@@ -2,6 +2,7 @@ package com.fruit.mall.firebase;
 
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Bucket;
 import com.google.firebase.cloud.StorageClient;
 import java.io.ByteArrayInputStream;
@@ -27,13 +28,33 @@ public class FireBaseService {
             return "File is Empty";
         }
 
+        // Firebase Storage의 버킷을 가져온다.
+        Bucket bucket = StorageClient.getInstance().bucket(firebaseBucket);
+
+        // 중복된 파일이름이 있으면 파일이름 뒤에 (+1) 씩 숫자를 붙인다. ex:) 키위.jpeg, 키위(1).jpeg, 키위(2).jpeg
         StringBuilder sb = new StringBuilder();
         sb.append(path);
         sb.append("/");
-        sb.append(fileName);
 
-        // Firebase Storage의 버킷을 가져온다.
-        Bucket bucket = StorageClient.getInstance().bucket(firebaseBucket);
+        String newFileName = fileName;
+        int duplicateCount = 0;
+
+        while (true) {
+            if (duplicateCount > 0) {
+                String ext = fileName.substring(fileName.lastIndexOf("."));
+                String name = fileName.substring(0, fileName.lastIndexOf("."));
+                newFileName = String.format("%s(%d)%s", name, duplicateCount, ext);
+            }
+            sb.setLength(path.length() + 1);
+            sb.append(newFileName);
+
+            BlobInfo blobInfo = BlobInfo.newBuilder(bucket.getName(), sb.toString()).build();
+            if (bucket.getStorage().get(blobInfo.getBlobId()) != null) {
+                duplicateCount++;
+            } else {
+                break;
+            }
+        }
 
         // MultipartFile을 바이트 배열로 변환하여 해당 파일의 내용을 가져온다.
         InputStream content = new ByteArrayInputStream(file.getBytes());
@@ -41,7 +62,7 @@ public class FireBaseService {
         // Firebase 스토리지에 파일 업로드. (업로드될 파일의 경로와 파일 이름, 파일의 내용, 파일 타입 )
         Blob blob = bucket.create(sb.toString(), content, file.getContentType());
 
-        log.info("이미지 저장 경로 {}", sb.toString());
+        log.info("이미지 저장 경로 {}", sb);
 
         // 업로드된 파일의 다운로드 URL을 반환
         return blob.getMediaLink();
