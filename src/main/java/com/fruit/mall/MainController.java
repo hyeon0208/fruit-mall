4695a54f88c450f5f573758168c3d8ef4d285fcf
Hyeon0208 <dmsgktn0208@gmail.com>
2023-08-20 20:50:11 +0900

    feat : 유저 메인 페이지 상품 목록 페이징 구현

--- src/main/java/com/fruit/mall/MainController.java
+++ src/main/java/com/fruit/mall/MainController.java
@@ -1,16 +1,23 @@
 package com.fruit.mall;
 
-import com.fruit.mall.user.UserService;
-import com.fruit.mall.user.dto.User;
+import com.fruit.mall.admin.image.ImageService;
+import com.fruit.mall.admin.product.Product;
+import com.fruit.mall.admin.product.ProductService;
+import com.github.pagehelper.PageInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MainController {
-    private final UserService userService;
+    private final ProductService productService;
+    private final ImageService imageService;
+
+    private final static String ON_SALE = "판매중";
 
     @GetMapping("favicon.ico")
     @ResponseBody
@@ -18,8 +25,20 @@ public class MainController {
     }
 
     @GetMapping("/")
-    public String home() {
-        return "index";
+    public String home(
+            Model model,
+            @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
+            @RequestParam(value = "pageSize", defaultValue = "9") Integer pageSize) {
+        PageInfo<Product> pageInfo = productService.getProductsByFilter(pageNum, pageSize, ON_SALE, null, null);
+        List<String> productImages = imageService.selectImageUrlsByStatus(ON_SALE);
+
+        System.out.println("뭐지 = " + productImages);
+        System.out.println("뭐지 = " + productImages.size());
+
+        model.addAttribute("pageInfo", pageInfo);
+        model.addAttribute("productImages", productImages);
+
+        return "user/index";
     }
 
     @GetMapping("/{pageName}")
