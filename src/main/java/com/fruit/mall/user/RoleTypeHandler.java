121f995f29d9138fccc4a57088747a7079e87b51
Hyeon0208 <dmsgktn0208@gmail.com>
2023-08-22 23:05:30 +0900

    update : 객체 구성 메서드 추가

--- src/main/java/com/fruit/mall/user/RoleTypeHandler.java
+++ src/main/java/com/fruit/mall/user/RoleTypeHandler.java
@@ -8,28 +8,43 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @MappedTypes(Role.class)
-public class RoleTypeHandler extends BaseTypeHandler<Role> {
+public class RoleTypeHandler implements TypeHandler<Role> {
 
     @Override
-    public void setNonNullParameter(PreparedStatement ps, int i, Role parameter, JdbcType jdbcType) throws SQLException {
+    public void setParameter(PreparedStatement ps, int i, Role parameter, JdbcType jdbcType) throws SQLException {
         ps.setString(i, parameter.getKey());
     }
 
     @Override
-    public Role getNullableResult(ResultSet rs, String columnName) throws SQLException {
-        String key = rs.getString(columnName);
-        return key == null ? null : Role.search(key);
+    public Role getResult(ResultSet rs, String columnName) throws SQLException {
+        String roleKey = rs.getString(columnName);
+        return getRole(roleKey);
     }
 
     @Override
-    public Role getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-        String key = rs.getString(columnIndex);
-        return key == null ? null : Role.search(key);
+    public Role getResult(ResultSet rs, int columnIndex) throws SQLException {
+        String roleKey = rs.getString(columnIndex);
+        return getRole(roleKey);
     }
 
     @Override
-    public Role getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
-        String key = cs.getString(columnIndex);
-        return key == null ? null : Role.search(key);
+    public Role getResult(CallableStatement cs, int columnIndex) throws SQLException {
+        String roleKey = cs.getString(columnIndex);
+        return getRole(roleKey);
+    }
+
+    // 실제 객체를 구성하는 메서드
+    private Role getRole(String key) {
+        Role role = null;
+        switch (key) {
+            case "ROLE_USER":
+                role = Role.USER;
+                break;
+
+            default:
+                role = Role.GUEST;
+                break;
+        }
+        return role;
     }
 }
