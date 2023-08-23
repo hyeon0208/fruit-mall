20fa49937225fd512b9e0869d8f09a2f694107ea
Hyeon0208 <dmsgktn0208@gmail.com>
2023-08-23 23:23:44 +0900

    feat : 상품 상세 페이지에 최근 본 상품 적용

--- src/main/java/com/fruit/mall/product/ProductDetailController.java
+++ src/main/java/com/fruit/mall/product/ProductDetailController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import static com.fruit.mall.user.MainController.recentProducts;
+
 @Controller
 @RequiredArgsConstructor
 public class ProductDetailController {
@@ -16,7 +18,7 @@ public class ProductDetailController {
     public String showDetail(@PathVariable("productId") Long productId, Model model) {
         ProductDetailForm productDetailForm = productService.selectProductDetailByProductId(productId);
         model.addAttribute("productDetailForm", productDetailForm);
-
+        model.addAttribute("recentProducts", recentProducts);
         return "user/detail";
     }
 }
