f514788208dfacb5304bb6fdbcf1ff7836bc25a6
Hyeon0208 <dmsgktn0208@gmail.com>
2023-08-24 18:28:32 +0900

    refactor : 미사용 import문 제거

--- src/main/java/com/fruit/mall/like/LikeService.java
+++ src/main/java/com/fruit/mall/like/LikeService.java
@@ -4,8 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 @Transactional
 @RequiredArgsConstructor
