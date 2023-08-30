dc94cfa64744f961acd3bc439bfad7afa509ed22
Hyeon0208 <dmsgktn0208@gmail.com>
2023-08-30 19:31:56 +0900

    refactor : JSONArray -> objectMapper로 JSON 파싱

--- src/main/java/com/fruit/mall/orders/OrderController.java
+++ src/main/java/com/fruit/mall/orders/OrderController.java
@@ -1,11 +1,13 @@
 package com.fruit.mall.orders;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fruit.mall.config.Login;
 import com.fruit.mall.config.SessionUser;
 import com.fruit.mall.orders.dto.OrderReqDto;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,16 +24,13 @@ public class OrderController {
     private final OrderService orderService;
 
     @GetMapping("/user/order")
-    public String orderProducts(@Login SessionUser sessionUser, Model model, @RequestParam String products) {
-        List<OrderReqDto> orderReqDtos = new ArrayList<>();
-        JSONArray productsArray = new JSONArray(products);
+    public String orderProducts(@Login SessionUser sessionUser, Model model, @RequestParam String products) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Long> productIds = objectMapper.readValue(products, new TypeReference<List<Long>>(){});
 
-        for (int i = 0; i < productsArray.length(); i++) {
-            Long productId = productsArray.getLong(i);
-            OrderReqDto orderReqDto = orderService.selectOrderInfosByProductId(productId);
-            orderReqDtos.add(orderReqDto);
-        }
-
+        List<OrderReqDto> orderReqDtos = productIds.stream()
+                .map(id -> orderService.selectOrderInfosByProductId(id))
+                .collect(Collectors.toList());
         model.addAttribute("products", orderReqDtos);
         model.addAttribute("totalSize", orderReqDtos.size());
         return "user/payment";
