20554ac50068ac4a1448a7062414f6060fcf2d94
Hyeon0208 <dmsgktn0208@gmail.com>
2023-08-31 15:42:03 +0900

    update : 배송지 필드 미입력시 알림 추가

--- src/main/java/com/fruit/mall/delivery/DeliveryController.java
+++ src/main/java/com/fruit/mall/delivery/DeliveryController.java
@@ -5,6 +5,8 @@ import com.fruit.mall.config.SessionUser;
 import com.fruit.mall.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,7 +19,12 @@ public class DeliveryController {
 
     @PostMapping("/delivery/add")
     @ResponseBody
-    public String insertDelivery(@Login SessionUser sessionUser, @RequestBody Delivery delivery) {
+    public String insertDelivery(@Login SessionUser sessionUser, @Validated @RequestBody Delivery delivery, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return bindingResult.getFieldError().getDefaultMessage();
+        }
+
         Long userId = userService.selectUserIdNByEmail(sessionUser.getEmail());
         deliveryService.insertDelivery(userId, delivery);
         return "success";
