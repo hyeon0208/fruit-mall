b720a6b7b59a718a0d6ce19ea90a6e24a1d24655
Hyeon0208 <dmsgktn0208@gmail.com>
2023-09-04 12:22:35 +0900

    refactor : 500번대 api 에러 코드 제거

--- src/main/java/com/fruit/mall/exception/ExControllerAdvice.java
+++ src/main/java/com/fruit/mall/exception/ExControllerAdvice.java
@@ -1,7 +1,6 @@
 package com.fruit.mall.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,11 +11,5 @@ public class ExControllerAdvice {
     @ExceptionHandler(IllegalArgumentException.class)
     public ErrorResult illegalExHandler(IllegalArgumentException e) {
         return new ErrorResult("BAD_INPUT", e.getMessage());
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler
-    public ErrorResult globalError(Exception e) {
-        return new ErrorResult("SERVER_EXCEPTION", "내부 오류 발생");
     }
 }
