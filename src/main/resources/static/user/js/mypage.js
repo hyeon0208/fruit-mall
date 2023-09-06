8bdede606d06657ee4c0cd532cb93f3195a6a03a
Hyeon0208 <dmsgktn0208@gmail.com>
2023-09-06 16:18:14 +0900

    feat : 리뷰보기, 재구매 기능 추가

---
+++ src/main/resources/static/user/js/mypage.js
@@ -0,0 +1,31 @@
+$(document).on("click", ".showReview", (e) => {
+    const productId = $(e.currentTarget).siblings(".reAddToCartBtn").data("product-id");
+    window.location.href=`/user/review/${productId}`
+});
+
+$(document).on("click", ".reAddToCartBtn", (e) => {
+    const btn = $(e.currentTarget);
+    const productId = btn.data("product-id");
+    const count = btn.data("count");
+
+    axios({
+        method: "post",
+        url: "/main/cart/add",
+        data: {
+            productId: productId,
+            productCount: count
+        },
+        dataType: "json",
+        headers: {'Content-Type': 'application/json'}
+    }).then((res) =>{
+        $(".txt04.right__modal.add__cart").show();
+        $('#closeCartModal').click(() => {
+            $(".txt04.right__modal.add__cart").hide();
+        });
+    }).catch((res) => {
+        $(".txt04.exist__mypage__cart").show();
+        $("#existCartModalClose").click(() => {
+            $(".txt04.exist__mypage__cart").hide();
+        });
+    });
+});
