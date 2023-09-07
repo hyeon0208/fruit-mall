d9db167c324c09efe3e955a41948a763ecdadcd3
Hyeon0208 <dmsgktn0208@gmail.com>
2023-09-07 20:56:47 +0900

    feat : 조건에 맞는 상품없을 경우 "최근 주문/배송 조회 내역이 없습니다." 표시

--- src/main/resources/static/user/js/mypage.js
+++ src/main/resources/static/user/js/mypage.js
@@ -201,117 +201,126 @@ function myPageOrderProductList() {
         const orderDetails = res.data.list;
         const pageInfo = res.data;
 
-        // .historyContents 내부의 ul 내부 내용 교체
-        const orderDetailList = orderDetails.map((orderDetail) => {
+        if (orderDetails.length == 0) {
+            const messageDiv = $("<div>")
+                .addClass("txt")
+                .css("text-align", "center")
+                .append($("<span>").css("font-size", "20px").text("최근 주문/배송 조회 내역이 없습니다."));
+            $(".historyContents ul").empty().append(messageDiv);
+            $('.pagination').empty();
+        } else {
+            // .historyContents 내부의 ul 내부 내용 교체
+            const orderDetailList = orderDetails.map((orderDetail) => {
 
-            // 배송지 정보 생성
-            const deliveryInfoDiv = $("<div>")
-                .addClass("deliveryInfo")
-                .css("display", "none")
-                .append(
-                    $("<br>"),
-                    $("<div>")
-                        .css({border:"1px #000000 solid", background:"#f7f5eb", padding:"8px"})
-                        .append($("<p>").text(`주문자 : ${orderDetail.receiverName}`))
-                        .append($("<p>").text(`연락처 : ${orderDetail.phoneNumber}`))
-                        .append($("<p>").text(`주소 : ${orderDetail.address}`))
-                        .append($("<p>").text(`배송요청사항 : ${orderDetail.orderRequired}`))
-                );
+                // 배송지 정보 생성
+                const deliveryInfoDiv = $("<div>")
+                    .addClass("deliveryInfo")
+                    .css("display", "none")
+                    .append(
+                        $("<br>"),
+                        $("<div>")
+                            .css({border:"1px #000000 solid", background:"#f7f5eb", padding:"8px"})
+                            .append($("<p>").text(`주문자 : ${orderDetail.receiverName}`))
+                            .append($("<p>").text(`연락처 : ${orderDetail.phoneNumber}`))
+                            .append($("<p>").text(`주소 : ${orderDetail.address}`))
+                            .append($("<p>").text(`배송요청사항 : ${orderDetail.orderRequired}`))
+                    );
 
-            // 재구매 버튼 생성
-            const reAddToCartBtn = $("<button>")
-                .addClass("reAddToCartBtn")
-                .attr('data-count', orderDetail.orderCount)
-                .attr('data-product-id', orderDetail.productId)
-                .attr('data-order-number', orderDetail.orderNumber)
-                .text("재구매");
+                // 재구매 버튼 생성
+                const reAddToCartBtn = $("<button>")
+                    .addClass("reAddToCartBtn")
+                    .attr('data-count', orderDetail.orderCount)
+                    .attr('data-product-id', orderDetail.productId)
+                    .attr('data-order-number', orderDetail.orderNumber)
+                    .text("재구매");
 
-            // 리뷰작성/리뷰보기 버튼 생성
-            let reviewButton;
-            if (!orderDetail.isWrite) {
-                reviewButton = $("<button>")
-                .addClass("writeReviewAtMyPage")
-                .text("리뷰작성");
-            } else {
-                reviewButton = $("<button>")
-                .addClass("showReview")
-                .css({"background-color":"#f1dacb", "color":"#c56206"})
-                .text("리뷰보기");
-            }
-
-            return $("<li>")
-                .append(
-                    $("<div>")
-                        .addClass("txt")
-                        .append(
-                            $("<span>").text(orderDetail.orderNumber),
-                            $("<span>").text(new Date(orderDetail.orderDate).toLocaleDateString("ko-KR", { year: 'numeric', month: '2-digit', day: '2-digit' }).replaceAll(". ", ".").substring(0, 10))
-                        ),
-                    $("<div>")
-                        .addClass("img")
-                        .append(
-                            $("<img>").attr({src: orderDetail.imageUrl}),
-                            $("<div>")
-                                .addClass('img__txt')
-                                .append(
-                                    $("<p>").text(orderDetail.productName),
-                                    $("<div>")
-                                        .addClass('group')
-                                        .append(
-                                            $("<span>").addClass('orange').text(`${(orderDetail.orderPrice * 1).toLocaleString()}원`),
-                                            $("<span>").text(`구매수량 ${orderDetail.orderCount}개`)
-                                        ),
-                                    // 배송지 확인 영역 추가
-                                    $('<div>')
-                                        .addClass('group2')
-                                        .html('<p>배송지 확인 <span class="material-symbols-outlined">double_arrow</span></p>'), deliveryInfoDiv
-                                )
-                        ),
-                    $("<div>", {class: 'buttons'}).append(reAddToCartBtn, reviewButton)
-                );
-        });
-        $(".historyContents ul").empty().append(orderDetailList);
-
-        // .pagination 클래스 태그 내부 내용 교체
-        const paginationDiv = $('.pagination').empty();
-        const totalData = pageInfo.pages; // 총 데이터 수
-        const pageNumberList = pageInfo.navigatepageNums; // 페이지 번호들의 순서를 담은 배열
-        const currentPage = pageInfo.pageNum;
-
-        // 이전 페이지 버튼
-        if (pageInfo.hasPreviousPage) {
-            const prevBtn = $("<a>")
-                .attr('href', '#')
-                .addClass("mypage-prevBtn")
-                .attr('value', pageInfo.prePage)
-                .html('<span class="material-symbols-outlined">chevron_left</span>');
-            paginationDiv.append(prevBtn);
-        }
-
-        // 페이지 번호 버튼
-        pageNumberList.forEach((pageNumber) => {
-            if (pageNumber <= totalData) {
-                const numberBtn = $("<a>")
-                    .text(pageNumber)
-                    .attr('href', '#')
-                    .addClass('mypage-numberBtn')
-                    .attr('value', pageNumber);
-
-                if (pageNumber === currentPage) {
-                    numberBtn.addClass("bold").addClass("large-text").css("font-size", "16px");
+                // 리뷰작성/리뷰보기 버튼 생성
+                let reviewButton;
+                if (!orderDetail.isWrite) {
+                    reviewButton = $("<button>")
+                        .addClass("writeReviewAtMyPage")
+                        .text("리뷰작성");
+                } else {
+                    reviewButton = $("<button>")
+                        .addClass("showReview")
+                        .css({"background-color":"#f1dacb", "color":"#c56206"})
+                        .text("리뷰보기");
                 }
-                paginationDiv.append(numberBtn);
-            }
-        });
 
-        // 다음 페이지 버튼
-        if (pageInfo.hasNextPage) {
-            const nextBtn = $("<a>")
-                .attr('href', '#')
-                .addClass("mypage-nextBtn")
-                .attr('value', pageInfo.nextPage)
-                .html('<span class="material-symbols-outlined">chevron_right</span>');
-            paginationDiv.append(nextBtn);
+                return $("<li>")
+                    .append(
+                        $("<div>")
+                            .addClass("txt")
+                            .append(
+                                $("<span>").text(orderDetail.orderNumber),
+                                $("<span>").text(new Date(orderDetail.orderDate).toLocaleDateString("ko-KR", { year: 'numeric', month: '2-digit', day: '2-digit' }).replaceAll(". ", ".").substring(0, 10))
+                            ),
+                        $("<div>")
+                            .addClass("img")
+                            .append(
+                                $("<img>").attr({src: orderDetail.imageUrl}),
+                                $("<div>")
+                                    .addClass('img__txt')
+                                    .append(
+                                        $("<p>").text(orderDetail.productName),
+                                        $("<div>")
+                                            .addClass('group')
+                                            .append(
+                                                $("<span>").addClass('orange').text(`${(orderDetail.orderPrice * 1).toLocaleString()}원`),
+                                                $("<span>").text(`구매수량 ${orderDetail.orderCount}개`)
+                                            ),
+                                        // 배송지 확인 영역 추가
+                                        $('<div>')
+                                            .addClass('group2')
+                                            .html('<p>배송지 확인 <span class="material-symbols-outlined">double_arrow</span></p>'), deliveryInfoDiv
+                                    )
+                            ),
+                        $("<div>", {class: 'buttons'}).append(reAddToCartBtn, reviewButton)
+                    );
+            });
+            $(".historyContents ul").empty().append(orderDetailList);
+
+            // .pagination 클래스 태그 내부 내용 교체
+            const paginationDiv = $('.pagination').empty();
+            const totalData = pageInfo.pages; // 총 데이터 수
+            const pageNumberList = pageInfo.navigatepageNums; // 페이지 번호들의 순서를 담은 배열
+            const currentPage = pageInfo.pageNum;
+
+            // 이전 페이지 버튼
+            if (pageInfo.hasPreviousPage) {
+                const prevBtn = $("<a>")
+                    .attr('href', '#')
+                    .addClass("mypage-prevBtn")
+                    .attr('value', pageInfo.prePage)
+                    .html('<span class="material-symbols-outlined">chevron_left</span>');
+                paginationDiv.append(prevBtn);
+            }
+
+            // 페이지 번호 버튼
+            pageNumberList.forEach((pageNumber) => {
+                if (pageNumber <= totalData) {
+                    const numberBtn = $("<a>")
+                        .text(pageNumber)
+                        .attr('href', '#')
+                        .addClass('mypage-numberBtn')
+                        .attr('value', pageNumber);
+
+                    if (pageNumber === currentPage) {
+                        numberBtn.addClass("bold").addClass("large-text").css("font-size", "16px");
+                    }
+                    paginationDiv.append(numberBtn);
+                }
+            });
+
+            // 다음 페이지 버튼
+            if (pageInfo.hasNextPage) {
+                const nextBtn = $("<a>")
+                    .attr('href', '#')
+                    .addClass("mypage-nextBtn")
+                    .attr('value', pageInfo.nextPage)
+                    .html('<span class="material-symbols-outlined">chevron_right</span>');
+                paginationDiv.append(nextBtn);
+            }
         }
     });
 }
