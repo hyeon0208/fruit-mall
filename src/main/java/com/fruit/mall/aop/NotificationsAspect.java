d90e8484faf5af4bf1fa7cb6f37c35f7d2a0cc41
Hyeon0208 <dmsgktn0208@gmail.com>
2023-09-20 14:27:12 +0900

    feat : 공통 헤더에 포함된 알림 아이콘 알림 데이터 AOP로 model에 전달

---
+++ src/main/java/com/fruit/mall/aop/NotificationsAspect.java
@@ -0,0 +1,55 @@
+package com.fruit.mall.aop;
+
+import com.fruit.mall.config.Login;
+import com.fruit.mall.config.SessionUser;
+import com.fruit.mall.notifications.NotificationsRepository;
+import com.fruit.mall.notifications.NotificationsService;
+import com.fruit.mall.notifications.dto.NotificationsResDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import java.util.List;
+
+import static com.fruit.mall.user.UserLoginController.LOGIN_USER;
+
+@Aspect
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class NotificationsAspect {
+    private final NotificationsService notificationsService;
+
+
+    @Around("execution(* com.fruit.mall..*Controller.*(..)) && @annotation(org.springframework.web.bind.annotation.GetMapping)")
+    public Object showNotifications(ProceedingJoinPoint joinPoint) throws Throwable {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        HttpSession session = request.getSession();
+        SessionUser sessionUser = (SessionUser) session.getAttribute(LOGIN_USER);
+
+        Object[] args = joinPoint.getArgs();
+
+        for (Object arg : args) {
+            if (arg instanceof Model) {
+                Model model = (Model) arg;
+                if (sessionUser != null){
+                    List<NotificationsResDto> notificationsList = notificationsService.selectMessagesByUserId(sessionUser.getUserIdNo());
+                    model.addAttribute("notificationsList", notificationsList);
+                    log.info("Notifications Aspect 적용 메서드 정보 : {}", joinPoint.getSignature());
+                }
+            }
+        }
+
+        return joinPoint.proceed(args);
+    }
+}
