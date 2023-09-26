dc753c1c7f17cb4beb64673a45bbd23df8504010
Hyeon0208 <dmsgktn0208@gmail.com>
2023-09-26 10:35:18 +0900

    update : docker 허브 계정명 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,9 @@
 sudo docker ps -a -q --filter "name=fruit-mall" | grep -q . && docker stop awsstudy && docker rm awsstudy | true
 
-sudo docker rmi hyeon0208/fruit-mall
+sudo docker rmi guswns0208/fruit-mall
 
-sudo docker pull hyeon0208/fruit-mall
+sudo docker pull guswns0208/fruit-mall
 
-docker run -d -p 8080:8080 --name fruit-mall hyeon0208/fruit-mall
+docker run -d -p 8080:8080 --name fruit-mall guswns0208/fruit-mall
 
 docker rmi -f $(docker images -f "dangling=true" -q) || true
