523cb0d916e348369cbb27c9d5dc80622562389a
Hyeon0208 <dmsgktn0208@gmail.com>
2023-09-27 14:30:07 +0900

    feat : elastic cache CONFIG 명령 에러 방지  keyspace notifications : no

---
+++ src/main/java/com/fruit/mall/config/SessionConfig.java
@@ -0,0 +1,17 @@
+package com.fruit.mall.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+@Configuration
+@EnableRedisHttpSession
+public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
+
+    @Bean
+    public ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
+    }
+}
